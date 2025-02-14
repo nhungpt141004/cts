f9c8eba7fea85d3f3011e73db6cdb706cf9ba4c8
nhungpt141004 <phamthihongnhung141004@gmail.com>
2025-02-14 11:13:47 +0700

    first commit

---
+++ src/component/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className=" bg-black flex flex-row">
+      <div className="flex flex-row">
+        <img src="/Image/logo.jpg" alt="logo" width={127} height={79} />
+        <div className=" bg-blue-600 w-[50px] h-5">nhung</div>
+      </div>
+    </header>
+  );
+};
+export default Header;
